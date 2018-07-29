756626c760dc1301f42e739a1001e07fd91cbe8d
Greg Lutostanski <gregory.lutostanski@canonical.com>
2018-07-29 16:08:26 +0200

    rust leap

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn is_leap_year(year: i32) -> bool {
+    if year % 400 == 0 {
+        return true;
+    }
+    if year % 100 == 0 {
+        return false;
+    }
+    if year % 4 == 0 {
+        return true;
+    }
+    return false;
+}
