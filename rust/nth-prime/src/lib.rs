b9b80ba590ce2e0f8b9e2e3ec1c38b0c14f3ad0d
Greg Lutostanski <gregory.lutostanski@canonical.com>
2018-08-04 09:20:45 +0200

    rust nth-prime

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn nth(n: u32) -> Option<u32> {
+    let mut primes = Vec::with_capacity(n as usize);
+    let mut current = 2_u32; // avoid 0 and 1 which are not prime
+
+    while primes.len() < n as usize {
+        if primes.iter().all(|&prime| current % prime != 0) {
+            primes.push(current);
+        }
+        current += 1;
+    }
+    return primes.pop();
+}
