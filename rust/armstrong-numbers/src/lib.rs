60663a6985470e719dfd4e7ff98c2b0b3b4b3eec
Greg Lutostanski <gregory.lutostanski@canonical.com>
2018-08-04 08:36:18 +0200

    rust armstrong numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,11 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut sum = 0u32;
+    let mut sum = Some(0u32);
     let mut current = num;
 
-    let digits = (num as f64).log10().ceil() as u32;
+    let digits = num.to_string().len() as u32;
     for _ in 1..=digits {
-        sum += (current % 10).pow(digits);
+        let add = (current % 10).pow(digits);
         current /= 10;
+
+        // use a checked_add to not panic if we overflow
+        sum = sum.unwrap().checked_add(add);
+        if sum.is_none() {
+            return false;
+        }
     }
-    return sum == num;
+    return sum.unwrap() == num;
 }
