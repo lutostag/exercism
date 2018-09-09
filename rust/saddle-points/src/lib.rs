d312194ebd6f742fd08b878d50732aa8fbe94dd2
Greg Lutostanski <greg.luto@gmail.com>
2018-09-09 20:21:55 +0200

    rust saddle-points: clean up a bit

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,58 +1,42 @@
 use std::cmp::Ordering;
 use std::collections::HashSet;
 
-fn find_indexes<F>(input: &[&u64], ordering: Ordering, reindex: F) -> Vec<(usize, usize)>
+fn find_indexes<F>(slice: &[u64], ordering: Ordering, reindex: F) -> Vec<(usize, usize)>
 where
     F: Fn(usize) -> (usize, usize),
 {
-    let mut min_or_max: Option<&u64> = None;
-    input
+    let min_or_max = match ordering {
+        Ordering::Greater => slice.iter().max(),
+        Ordering::Less => slice.iter().min(),
+        _ => None,
+    };
+
+    slice
         .iter()
         .enumerate()
-        .fold(
-            Vec::<(usize, usize)>::new(),
-            |mut vector, (idx, &x)| match min_or_max {
-                None => {
-                    min_or_max = Some(x);
-                    vec![reindex(idx)]
-                }
-                Some(current) => match x.cmp(current) {
-                    order if order == ordering => {
-                        min_or_max = Some(x);
-                        vec![reindex(idx)]
-                    }
-                    Ordering::Equal => {
-                        vector.push(reindex(idx));
-                        vector
-                    }
-                    _ => vector,
-                },
-            },
-        )
+        .filter_map(|(idx, &x)| {
+            if &x == min_or_max.unwrap() {
+                return Some(reindex(idx));
+            }
+            None
+        })
+        .collect()
 }
 
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut maxs: Vec<(usize, usize)> = Vec::new();
-    let mut mins: HashSet<(usize, usize)> = HashSet::new();
+    let mut maxs = Vec::<(usize, usize)>::new();
+    let mut mins = HashSet::<(usize, usize)>::new();
 
-    let flat: Vec<&u64> = input.iter().flat_map(|x| x).collect();
-    let (height, width) = (input.len(), flat.len() / input.len());
-    if height == 0 || width == 0 {
-        return maxs;
-    }
-
-    for (row_idx, row) in flat.chunks(width).enumerate() {
+    for (row_idx, row) in input.iter().enumerate() {
         maxs.extend(find_indexes(&row, Ordering::Greater, |i| (row_idx, i)));
     }
 
-    for col_idx in 0..width {
-        let col: Vec<&u64> = (col_idx..flat.len())
-            .step_by(width)
-            .map(|x| flat[x])
-            .collect();
+    let (rows, cols) = (input.len(), input[0].len());
+    for col_idx in 0..cols {
+        let col: Vec<u64> = (0..rows).map(|row_idx| input[row_idx][col_idx]).collect();
         mins.extend(find_indexes(&col, Ordering::Less, |i| (i, col_idx)));
     }
 
-    maxs.retain(|&x| mins.contains(&x));
+    maxs.retain(|&max| mins.contains(&max));
     maxs
 }
