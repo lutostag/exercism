3cb0b519e5867ab450168a6cc626c3eeb77e329b
Greg Lutostanski <greg.luto@gmail.com>
2018-09-09 18:04:44 +0200

    rust proverb: try with functional methods

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,16 +1,13 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    if list.is_empty() {
-        return String::new();
+    let mut proverb: Vec<String> = list
+        .iter()
+        .zip(list.iter().skip(1))
+        .map(|(a, b)| format!("For want of a {} the {} was lost.", a, b))
+        .collect();
+    
+    match list.first() {
+        None => return String::new(),
+        Some(item) => proverb.push(format!("And all for the want of a {}.", item)),
     }
-
-    let mut proverb = Vec::with_capacity(list.len());
-    for i in 0..list.len() - 1 {
-        proverb.push(format!(
-            "For want of a {} the {} was lost.",
-            list[i],
-            list[i + 1]
-        ));
-    }
-    proverb.push(format!("And all for the want of a {}.", list[0]));
-    return proverb.join("\n");
+    proverb.join("\n")
 }
