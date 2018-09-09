e6df6c07bc63b13671ceb4d0effcb7d2eb1510a5
Greg Lutostanski <greg.luto@gmail.com>
2018-09-09 18:12:59 +0200

    rust grains: remove return

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -2,7 +2,7 @@ pub fn square(s: u32) -> u64 {
     if s < 1 || s > 64 {
         panic!("Square must be between 1 and 64");
     }
-    return 1_u64 << (s - 1); // use bitshift, same as 2 raised to (s - 1)
+    1_u64 << (s - 1) // use bitshift, same as 2 raised to (s - 1)
 }
 
 pub fn total() -> u64 {
