0d8b1f47d728ced4453132534603f12960679bbd
Greg Lutostanski <greg.luto@gmail.com>
2018-09-09 18:15:28 +0200

    rust gigaseconds: remove return

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -4,5 +4,5 @@ use chrono::{DateTime, Duration, Utc};
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
     let gigaseconds = Duration::seconds(1_000_000_000);
-    return start + gigaseconds;
+    start + gigaseconds
 }
