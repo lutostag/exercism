181c205de5deeeb06cb89f458cc650cd996811f6
Greg Lutostanski <greg.luto@gmail.com>
2018-09-19 23:07:40 +0200

    rust reverse-string: cleanup

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,10 +3,5 @@ extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut reversed = String::with_capacity(input.len());
-    input
-        .graphemes(true)
-        .rev()
-        .for_each(|cluster| reversed.push_str(cluster));
-    reversed
+    input.graphemes(true).rev().collect()
 }
