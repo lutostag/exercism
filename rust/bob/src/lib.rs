db771f17578a3dfea87b7024bbaae9a0156386f7
Greg Lutostanski <greg.luto@gmail.com>
2018-09-19 23:13:34 +0200

    rust bob: else if implementation

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -8,11 +8,16 @@ fn question(message: &str) -> bool {
 
 pub fn reply(message: &str) -> &str {
     let message = message.trim_right();
-    match message {
-        _ if yelling(message) && question(message) => "Calm down, I know what I'm doing!",
-        _ if yelling(message) => "Whoa, chill out!",
-        _ if question(message) => "Sure.",
-        _ if message.is_empty() => "Fine. Be that way!",
-        _ => "Whatever.",
+
+    if yelling(message) && question(message) {
+        "Calm down, I know what I'm doing!"
+    } else if yelling(message) {
+        "Whoa, chill out!"
+    } else if question(message) {
+        "Sure."
+    } else if message.is_empty() {
+        "Fine. Be that way!"
+    } else {
+        "Whatever."
     }
 }
