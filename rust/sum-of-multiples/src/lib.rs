7f8b301a4d5ea314fc8bd1ca2e07423a10080b07
Greg Lutostanski <greg.luto@gmail.com>
2018-09-27 19:59:37 +0200

    rust sum-of-multiples: using iterators

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,12 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut sum = 0;
-    for i in 1..limit {
-        for factor in factors {
-            if i % factor == 0 {
-                sum += i;
-                break;
-            }
-        }
-    }
-    sum
+    (1..limit)
+        .filter(|num| factors.iter().any(|factor| num % factor == 0))
+        .sum()
 }
