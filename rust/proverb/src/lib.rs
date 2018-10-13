117062307831a5bbf747f513bf0d9c17591de4af
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-10-13 14:01:47 +0200

    rus proverb: clean up a bit more

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,11 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    let mut proverb: Vec<String> = list
-        .iter()
-        .zip(list.iter().skip(1))
-        .map(|(a, b)| format!("For want of a {} the {} was lost.", a, b))
-        .collect();
+    let mut proverb = String::new();
     
-    match list.first() {
-        None => return String::new(),
-        Some(item) => proverb.push(format!("And all for the want of a {}.", item)),
+    for (a, b) in list.iter().zip(list.iter().skip(1)) {
+        proverb += &format!("For want of a {} the {} was lost.\n", a, b);
     }
-    proverb.join("\n")
+    if let Some(first) = list.first() {
+        proverb += &format!("And all for the want of a {}.", first);
+    }
+    proverb
 }
