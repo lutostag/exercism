32a7209a1fca7ae96e1b7893af6abd90d4826a6c
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-11-04 20:27:58 +0000

    rust largest-series-product: try using ok_or again

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -7,14 +7,15 @@ pub enum Error {
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
     if span == 0 {
         return Ok(1);
-    } else if let Some(c) = string_digits.chars().find(|&c| !c.is_digit(10)) {
-        return Err(Error::InvalidDigit(c));
     }
 
     let digits: Vec<u64> = string_digits
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as u64)
-        .collect();
+        .map(|c| c.to_digit(10)
+             .ok_or(Error::InvalidDigit(c))
+             .map(|d| d as u64))
+        .collect::<Result<_, _>>()?;
+
     digits
         .windows(span)
         .map(|window| window.iter().product())
