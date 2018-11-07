e0642e046ae66b82ba5359628e458699f193a246
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-11-07 21:50:22 +0000

    rust triangle: less cloning

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -27,16 +27,16 @@ where
 
 impl<'a, T: 'a> Triangle<T>
 where
-    T: std::cmp::Ord
+    T: std::ops::Add<T, Output = T>
+        + std::cmp::Ord
         + std::iter::Sum<T>
-        + std::ops::Add<T, Output = T>
         + std::marker::Copy
         + std::convert::From<u8>,
     &'a T: std::cmp::PartialOrd,
 {
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
-        let max = sides.iter().cloned().max().unwrap();
-        let min = sides.iter().cloned().min().unwrap();
+        let max = sides.iter().max().unwrap().clone();
+        let min = sides.iter().min().unwrap().clone();
         let twice_max = max + max;
         let sum = sides.iter().cloned().sum();
         let zero = T::from(0u8);
