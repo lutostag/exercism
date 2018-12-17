bfafed72d9440ef7f2416995cdeed376094da7ef
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-12-16 21:51:27 -0600

    rust queen-attack

---
+++ rust/queen-attack/src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
+
+#[derive(Debug)]
+pub struct Queen {
+    position: ChessPosition,
+}
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        if rank < 0 || file < 0 || rank > 7 || file > 7 {
+            None
+        } else {
+            Some(ChessPosition { rank, file })
+        }
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Queen { position }
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        self.position.rank == other.position.rank
+            || self.position.file == other.position.file
+            || (self.position.rank - other.position.rank).abs()
+                == (self.position.file - other.position.file).abs()
+    }
+}
