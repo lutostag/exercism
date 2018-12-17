1ecb6a3d2aabce9e04f3c2117631f4181de7a442
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2018-12-16 18:08:11 -0600

    rust diamond: try a slower, maybe more clear approach

--- rust/diamond/src/lib.rs
+++ rust/diamond/src/lib.rs
@@ -1,24 +1,18 @@
 extern crate char_iter;
 
-fn offset(c: char) -> usize {
-    if !(c.is_ascii_alphabetic() && c.is_ascii_uppercase()) {
-        panic!("not an expected char: {}", c);
-    }
-    c as usize - 'A' as usize
-}
-
 pub fn get_diamond(until: char) -> Vec<String> {
     let mut diamond = Vec::new();
-    let middle_str = offset(until);
-    let blank = " ".repeat(middle_str * 2 + 1);
+    let back: String = char_iter::new('A', until).collect();
+    let front: String = back.chars().rev().collect();
+    let mut full = String::from(front);
+    full.pop(); // remove second copy of 'A'
+    full.push_str(&back);
 
     for c in char_iter::new('A', until) {
-        let mut row = blank.clone();
-        let offset = offset(c);
-        row.replace_range(middle_str + offset..middle_str + offset + 1, &c.to_string());
-        row.replace_range(middle_str - offset..middle_str - offset + 1, &c.to_string());
+        let row: String = full.chars().map(|l| if l == c { c } else { ' ' }).collect();
         let middle_vec = diamond.len() / 2;
         if c != until {
+            // Do not add 2-copies of the last character, only one
             diamond.insert(middle_vec, row.clone());
         }
         diamond.insert(middle_vec, row);
