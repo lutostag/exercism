62309971e861c5cc58c79b74e3ae651ca0aa6203
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-03-14 12:43:33 +0100

    rust bracket-push: first attempt

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -1,6 +1,29 @@
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate maplit;
+
+use std::collections::{HashMap, HashSet};
+
+lazy_static! {
+    static ref OPEN_BRACKETS: HashSet<char> = hashset! {'{', '(', '[',};
+    static ref CLOSE_BRACKETS: HashMap<char, char> = hashmap! {
+        '}' => '{',
+        ')' => '(',
+        ']' => '[',
+    };
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = Vec::new();
+    for c in string.chars() {
+        if OPEN_BRACKETS.contains(&c) {
+            stack.push(c);
+        } else if let Some(&m) = CLOSE_BRACKETS.get(&c) {
+            if Some(m) != stack.pop() {
+                return false;
+            }
+        }
+    }
+    stack.is_empty()
 }
