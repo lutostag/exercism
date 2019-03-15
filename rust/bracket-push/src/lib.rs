61646ea70e20e4235cdf8e0156163ce55e29af6f
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-03-15 17:16:17 -0500

    rust bracket-push: increase readability

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -6,12 +6,13 @@ extern crate maplit;
 use std::collections::{HashMap, HashSet};
 
 lazy_static! {
-    static ref OPEN_BRACKETS: HashSet<char> = hashset! {'{', '(', '[',};
     static ref CLOSE_BRACKETS: HashMap<char, char> = hashmap! {
         '}' => '{',
         ')' => '(',
         ']' => '[',
     };
+    static ref OPEN_BRACKETS: HashSet<char> =
+        CLOSE_BRACKETS.values().into_iter().cloned().collect();
 }
 
 pub fn brackets_are_balanced(string: &str) -> bool {
@@ -19,8 +20,8 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     for c in string.chars() {
         if OPEN_BRACKETS.contains(&c) {
             stack.push(c);
-        } else if let Some(&m) = CLOSE_BRACKETS.get(&c) {
-            if Some(m) != stack.pop() {
+        } else if CLOSE_BRACKETS.contains_key(&c) {
+            if stack.pop() != CLOSE_BRACKETS.get(&c).cloned() {
                 return false;
             }
         }
