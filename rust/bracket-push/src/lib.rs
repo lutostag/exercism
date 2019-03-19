bc15fb85b940b98e3d1b0027d1ef736624a966bd
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-03-18 21:55:07 -0500

    rust bracket-push: do without hashmap

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -1,30 +1,15 @@
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate maplit;
-
-use std::collections::{HashMap, HashSet};
-
-lazy_static! {
-    static ref CLOSE_BRACKETS: HashMap<char, char> = hashmap! {
-        '}' => '{',
-        ')' => '(',
-        ']' => '[',
-    };
-    static ref OPEN_BRACKETS: HashSet<char> =
-        CLOSE_BRACKETS.values().into_iter().cloned().collect();
-}
-
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack = Vec::new();
     for c in string.chars() {
-        if OPEN_BRACKETS.contains(&c) {
-            stack.push(c);
-        } else if CLOSE_BRACKETS.contains_key(&c) {
-            if stack.pop() != CLOSE_BRACKETS.get(&c).cloned() {
-                return false;
-            }
-        }
+        match c {
+            '{' | '(' | '[' => stack.push(c),
+            '}' | ')' | ']' if stack.is_empty() => return false,
+            '}' | ')' | ']' => match (stack.pop().unwrap(), c) {
+                ('{', '}') | ('(', ')') | ('[', ']') => (),
+                _ => return false,
+            },
+            _ => (),
+        };
     }
     stack.is_empty()
 }
