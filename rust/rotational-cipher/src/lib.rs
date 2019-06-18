abcac6df9ee6cff38054736c50060470c184d261
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-06-17 20:36:04 -0500

    rust rotational-cipher: first solution

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,7 +1,16 @@
+#![feature(euclidean_division)]
+use std::collections::HashMap;
+
+const ALPHABET: &'static str = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    let key: usize = key.rem_euclid(26) as usize * 2; // multiply for upper and lower
+    let rotated = ALPHABET.chars().skip(key).chain(ALPHABET.chars().take(key));
+    let cipher: HashMap<char, char> = ALPHABET.chars().zip(rotated).collect();
+
+    input
+        .clone()
+        .chars()
+        .map(|c| cipher.get(&c).unwrap_or(&c).clone())
+        .collect()
 }
