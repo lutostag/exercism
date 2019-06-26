4459cb4ea3d28896862f5d42dcccf39a075ca457
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-06-26 07:30:31 -0500

    rust binary-search: first implementation

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    fn find_recursive(array: &[i32], key: i32, offset: usize) -> Option<usize> {
+        let pivot = array.len() / 2;
+
+        match array.len() {
+            0 => None,
+            1 if key == array[0] => Some(offset),
+            1 => None,
+            _ if key == array[pivot] => Some(offset + pivot),
+            _ if key < array[pivot] => find_recursive(&array[..pivot], key, offset),
+            _ => find_recursive(&array[pivot..], key, offset + pivot),
+        }
+    }
+    find_recursive(array, key, 0)
 }
