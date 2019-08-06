232b3c17a83e3d8c8479cfa77965e4d91434b0e8
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-08-06 18:18:49 -0500

    rust say: cleanup

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -1,27 +1,8 @@
-const SECTIONS: [&'static str; 7] = [
-    "",
-    " thousand",
-    " million",
-    " billion",
-    " trillion",
-    " quadrillion",
-    " quintillion",
-];
-
-fn encode_larger(n: u64) -> String {
-    let mut sections = Vec::with_capacity(SECTIONS.len());
-    let mut n = n;
-    for section_name in SECTIONS.iter() {
-        let section = n % 1000;
-        if section != 0 {
-            sections.insert(0, format!("{}{}", encode(section), section_name));
-        }
-        n /= 1000;
-    }
-    sections.join(" ")
-}
+use std::u64::MAX;
 
 pub fn encode(n: u64) -> String {
+    let larger = |base, string| format!("{} {} {}", encode(n / base), string, encode(n % base));
+
     match n {
         0 => format!("zero"),
         1 => format!("one"),
@@ -37,9 +18,12 @@ pub fn encode(n: u64) -> String {
         11 => format!("eleven"),
         12 => format!("twelve"),
         13 => format!("thirteen"),
+        14 => format!("fourteen"),
         15 => format!("fifteen"),
+        16 => format!("sixteen"),
+        17 => format!("seventeen"),
         18 => format!("eighteen"),
-        13...19 => format!("{}teen", encode(n % 10)),
+        19 => format!("nineteen"),
         20 => format!("twenty"),
         30 => format!("thirty"),
         40 => format!("forty"),
@@ -48,9 +32,14 @@ pub fn encode(n: u64) -> String {
         70 => format!("seventy"),
         80 => format!("eighty"),
         90 => format!("ninety"),
-        21...99 => format!("{}-{}", encode(n - (n % 10)), encode(n % 10)),
-        100...900 if n % 100 == 0 => format!("{} hundred", encode(n / 100)),
-        100...999 => format!("{} {}", encode(n - (n % 100)), encode(n % 100)),
-        _ => encode_larger(n),
+        21...99 => format!("{}-{}", encode(n / 10 * 10), encode(n % 10)),
+        1_000_000_000_000_000_000...MAX => larger(1_E18 as u64, "quintillion"),
+        1_000_000_000_000_000...MAX => larger(1_E15 as u64, "quadrillion"),
+        1_000_000_000_000...MAX => larger(1_E12 as u64, "trillion"),
+        1_000_000_000...MAX => larger(1_E9 as u64, "billion"),
+        1_000_000...MAX => larger(1_E6 as u64, "million"),
+        1_000...MAX => larger(1_E3 as u64, "thousand"),
+        100...MAX => larger(1_E2 as u64, "hundred"),
     }
+    .replace(" zero", "")
 }
