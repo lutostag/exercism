b977c5b12b1d4d068621ed808677747dbb98a82a
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-08-05 21:04:42 -0500

    rust binary-search: greater than pivot slice cleanup

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
+    if array.is_empty() {
+        return None;
+    }
     let pivot = array.len() / 2;
 
-    match array.len() {
-        1 if key == array[0] => Some(0),
-        0 | 1 => None,
-        _ if key == array[pivot] => Some(pivot),
-        _ if key < array[pivot] => find(&array[..pivot], key),
-        _ => find(&array[pivot..], key).map(|o| o + pivot),
+    match array[pivot] {
+        value if key == value => Some(pivot),
+        value if key < value => find(&array[..pivot], key),
+        _ => find(&array[pivot + 1..], key).map(|o| o + pivot + 1),
     }
 }
