07c9b8e2a057101f923124a5f1ce83240eaeb11b
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-08-10 20:00:19 -0500

    rust simple-linked-list: do not iterate so much

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,5 +1,3 @@
-use std::mem;
-
 type NodePointer<T> = Option<Box<Node<T>>>;
 
 pub struct Node<T> {
@@ -7,17 +5,9 @@ pub struct Node<T> {
     next: NodePointer<T>,
 }
 
-#[derive(Default)]
 pub struct SimpleLinkedList<T> {
     head: NodePointer<T>,
     count: usize,
-}
-
-fn create<T>(element: T) -> NodePointer<T> {
-    Some(Box::new(Node {
-        data: element,
-        next: None,
-    }))
 }
 
 fn data<T>(pointer: &NodePointer<T>) -> Option<&T> {
@@ -26,10 +16,6 @@ fn data<T>(pointer: &NodePointer<T>) -> Option<&T> {
 
 fn next<T>(pointer: &NodePointer<T>) -> &NodePointer<T> {
     &pointer.as_ref().unwrap().next
-}
-
-fn next_mut<T>(pointer: &mut NodePointer<T>) -> &mut NodePointer<T> {
-    &mut pointer.as_mut().unwrap().next
 }
 
 impl<T> SimpleLinkedList<T> {
@@ -49,27 +35,21 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn push(&mut self, element: T) {
-        let mut current = &mut self.head;
-        while current.is_some() {
-            current = next_mut(current);
-        }
-        self.count += 1;
-        mem::replace(current, create(element));
-    }
-
-    pub fn push_front(&mut self, element: T) {
-        let old_head = mem::replace(&mut self.head, create(element));
-        *next_mut(&mut self.head) = old_head;
+        self.head = Some(Box::new(Node {
+            data: element,
+            next: self.head.take(),
+        }));
         self.count += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        let mut current = &mut self.head;
-        while current.is_some() && next(current).is_some() {
-            current = next_mut(current);
+        if let Some(node) = self.head.take() {
+            self.count -= 1;
+            self.head = node.next;
+            Some(node.data)
+        } else {
+            None
         }
-        self.count = self.count.checked_sub(1).unwrap_or(0);
-        current.take().map(|node| (*node).data)
     }
 
     pub fn peek(&self) -> Option<&T> {
@@ -80,10 +60,9 @@ impl<T> SimpleLinkedList<T> {
 impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(&self) -> SimpleLinkedList<T> {
         let mut list: SimpleLinkedList<T> = SimpleLinkedList::new();
-
         let mut current = &self.head;
         while current.is_some() {
-            list.push_front(data(current).unwrap().clone());
+            list.push(data(current).unwrap().clone());
             current = next(current);
         }
         list
